78c19607ce6a53124951108dccffb8ad7a0f69d6
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-01-08 00:57:57 -0600

    update test

--- fases/fase3/test/test.f90
+++ fases/fase3/test/test.f90
@@ -1,43 +1,31 @@
-program test_parser
-    use parser
-    implicit none
+program test
+	use parser
+	implicit none
+	character(len=100) :: filename
+	character(len=:), allocatable :: inputstr
+	integer :: u, len
+	logical :: exists
+	type(node), pointer :: stack => null() 
 
-    character(len=100) :: filename
-    character(len=:), allocatable :: inputstr
-    integer :: len
-    logical :: exists
-    integer :: parse_result
+	if (command_argument_count() == 0) then
+		print *, "error: no input file"
+		stop
+	end if
 
-    ! Check if the filename argument is provided
-    if (command_argument_count() == 0) then
-        print *, "error: no input file"
-        stop
-    end if
+	call get_command_argument(1, filename)
 
-    ! Get the filename from the command line
-    call get_command_argument(1, filename)
+	inquire(file=filename, exist=exists, size=len)
+	if (exists) then
+		open (1, file=filename, status='old', action='read', access='stream', form='unformatted')
+		allocate (character(len=len) :: inputstr)
+        read (1) inputstr
+		stack => parse(inputstr)
+		call show()
+	else
+		print *, "error: file is not present"
+		stop
+	end if
 
-    ! Check if the file exists and get its size
-    inquire(file=filename, exist=exists, size=len)
-    if (exists) then
-        if (len == 0) then
-            print *, "error: input file is empty"
-            stop
-        end if
+	close(u)
 
-        ! Open the file and read its contents
-        open(unit=1, file=filename, status='old', action='read', access='stream', form='unformatted')
-        allocate(character(len=len) :: inputstr)
-        read(1) inputstr
-        close(1)
-
-        ! Call the parse function with the input string
-        parse_result = parse(inputstr)
-
-        ! Print the result of the parse function
-        print *, "Parse result:", parse_result
-    else
-        print *, "error: file is not present"
-        stop
-    end if
-end program test_parser
+end program test
